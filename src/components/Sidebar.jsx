9606bfb0e4c2dc2d0d9903d61ee99cd66dc1d3bb
Tarun Mandhan <mandhantarun@gmail.com>
2023-07-05 12:11:23 +0530

    add icon & <NavLink to , key , >

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { FaHome } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    icon: <FaHome />,
+  },
+];
 
 const Sidebar = ({ children }) => {
   return (
-    <>
-      <div className="main-container">
-        <motion.div
-          animate={{ width: "200px" }}
-          className="sidebar"
-        ></motion.div>
-        <main>{children}</main>
-      </div>
-    </>
+    <div className="main-container">
+      <motion.div animate={{ width: "200px" }} className="sidebar">
+        <section className="routes">
+          {routes.map((route) => (
+            <NavLink to={route.path} key={route.name}>
+              <div className="icon">{route.icon}</div>
+            </NavLink>
+          ))}
+        </section>
+      </motion.div>
+      <main>{children}</main>
+    </div>
   );
 };
 
